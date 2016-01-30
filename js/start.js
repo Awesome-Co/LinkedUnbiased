0291a7cf6c708b37360393f09053fc02df2a38a7
David Morrison <dave@davemorro.com>
2016-01-30 12:07:07 -0500

    Removed minified version and resolved comment issue.

--- js/start.js
+++ js/start.js
@@ -1,5 +1,5 @@
 var link = document.createElement('link');
-link.href =  chrome.extension.getURL('css/linkedin.min.css');
+link.href =  chrome.extension.getURL('css/linkedin.css');
 link.rel = 'stylesheet';
 var head = document.getElementsByTagName('head')[0] || document.documentElement;
 head.insertBefore(link, head.firstChild);
